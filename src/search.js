92f1f8ccfd9ce18ccf0de93751c9e3fc1ed66ea7
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-05-17 22:52:38 +0200

    feat: add comments

--- src/search.js
+++ src/search.js
@@ -1,17 +1,26 @@
 const fs = require("fs");
-const readline = require("readline");
 
-const filePath = "./file.csv";
-const column = 2;
-const key = "Alberto";
+// Read arguments from command line
+const args = process.argv;
+
+// Check if the number of arguments is correct
+if(args.length != 5) {
+  console.log("Usage: node src/search.js <file_path> <column> <key>");
+  process.exit(1);
+}
+
+const filePath = args[2];
+const column = args[3];
+const key =  args[4];
+
 // Read a file line by line synchronously
 const fileContent = fs.readFileSync(filePath, "utf8");
 
+// Search for the first line with key in the column
 const found = fileContent
   .toString()
   .split(/\r?\n/)
-  .find((line) => line.split(",")[column] == key);
+  .find((line) => line.split(/,|;/)[column] == key);
 
-if (found) {
-  console.log(found);
-}
+// Print the result
+console.log(found ? found : "Not found");
