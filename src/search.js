abdecebd7b980ca54a61e6b6c6162f66c8c6887b
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-05-17 23:05:38 +0200

    feat: cleanup

--- src/search.js
+++ src/search.js
@@ -4,19 +4,19 @@ const fs = require("fs");
 const args = process.argv;
 
 // Check if the number of arguments is correct
-if(args.length != 5) {
+if (args.length != 5) {
   console.log("Usage: node src/search.js <file_path> <column> <key>");
   process.exit(1);
 }
 
 const filePath = args[2];
 const column = args[3];
-const key =  args[4];
+const key = args[4];
 
 // Read a file line by line synchronously
 const fileContent = fs.readFileSync(filePath, "utf8");
 
-// Search for the first line with key in the column
+// Search for the FIRST line with key in the column
 const found = fileContent
   .toString()
   .split(/\r?\n/)
